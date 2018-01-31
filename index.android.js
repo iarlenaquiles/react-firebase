1e642f57608fb1edc684eeb47d96b6ef2749214f
Iarlen <iarlensilva775@gmail.com>
2018-01-31 18:25:11 -0300

    Configurando o firebase

--- index.android.js
+++ index.android.js
@@ -5,6 +5,20 @@ import firebase from 'firebase';
 
 export default class App extends Component {
 
+  componentWillMount() {
+
+    var config = {
+      apiKey: "AIzaSyBhc5EqO1Rnp3IepnnF9JKN-ErmzDE9bbw",
+      authDomain: "fir-teste-ac98e.firebaseapp.com",
+      databaseURL: "https://fir-teste-ac98e.firebaseio.com",
+      projectId: "fir-teste-ac98e",
+      storageBucket: "fir-teste-ac98e.appspot.com",
+      messagingSenderId: "887666779936"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
